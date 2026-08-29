40e23eb2e88af70a153cfbd09a32eeec5a31d3d5
Arsh Mansuri <arsh.amin.may.2006@gmail.com>
2026-08-29 16:55:05 +0530

    added the PATCH api

--- notes/src/app.js
+++ notes/src/app.js
@@ -1,38 +1,49 @@
-const express=require("express");
+const express = require("express");
 
-const app=express();
+const app = express();
 
-app.use(express.json())
+app.use(express.json());
 
-let notes=[];
+let notes = [];
 
-app.post('/notes',(req,res)=>{
-    notes.push(req.body);
+app.post("/notes", (req, res) => {
+  notes.push(req.body);
 
-    res.status(201).json({
-        message:'notes received successfully'
-    })
-})
+  res.status(201).json({
+    message: "notes received successfully",
+  });
+});
 
-app.get('/notes',(req,res)=>{
-    res.status(200).json({
-        notes:notes,
-        message:'notes fetched successfully'
-    })
-})
+app.get("/notes", (req, res) => {
+  res.status(200).json({
+    notes: notes,
+    message: "notes fetched successfully",
+  });
+});
 
-app.delete('/notes/:index',(req,res)=>{
-    const index=req.params.index;
+app.delete("/notes/:index", (req, res) => {
+  const index = req.params.index;
 
-    delete notes[index]
+  delete notes[index];
 
-    res.status(200).json({
-        message:'note deleted successfully'
-    })
-})
+  res.status(200).json({
+    message: "note deleted successfully",
+  });
+});
 
-app.get('/home',(req,res)=>{
-    res.send('Hello i am the home')
-})
+app.patch("/notes/:index", (req, res) => {
+  const index = req.params.index;
+  const description = req.body.description;
 
-module.exports=app;
+  notes[index].description = description;
+
+  res.status(200).json({
+    message: "note updated successfully",
+  });
+});
+
+app.get("/home", (req, res) => {
+  res.send("Hello i am the home");
+});
+
+module.exports = app;
